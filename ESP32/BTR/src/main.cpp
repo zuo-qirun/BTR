beab71e78cfb1e83f582bc3f2c50e2b821f26118
zuo-qirun <zuoqirun2009@126.com>
2026-01-31 18:54:03 +0800

    init

--- ESP32/BTR/src/main.cpp
+++ ESP32/BTR/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#define NUM_LEDS 1
+#define DATA_PIN 48
+CRGB leds[NUM_LEDS];
+
 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Green;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Blue;
+  FastLED.show();
+  delay(1000);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
